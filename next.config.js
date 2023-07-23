176fdfae917ed626c135bb3f47d34d95c6257cd7
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-07-23 20:43:46 +0100

    for redirect to homepage

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/_error",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
