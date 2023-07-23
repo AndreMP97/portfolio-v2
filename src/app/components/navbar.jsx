69e25d9c335d568b4a569339d4860a3494c089ce
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-07-23 22:03:34 +0100

    add framer motion to mobile navbar

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { CSSTransition } from "react-transition-group";
 import { Squash as Hamburger } from "hamburger-react";
+import { CurrentBreakpoint } from "../utils/breakpoints";
 
 const Navbar = ({ navLinks }) => {
+  const breakpoint = CurrentBreakpoint();
+  const mobileBreakpoints = ["xs", "sm"];
+
   const [nav, setNav] = useState(false);
+  const [border, setBorder] = useState(false);
 
   const handleClick = (ref) => {
     if (ref) {
@@ -14,6 +18,36 @@ const Navbar = ({ navLinks }) => {
     }
   };
 
+  const listVariants = {
+    open: {
+      clipPath: "inset(0% 0% 0% 0% round 10px)",
+      transition: {
+        type: "spring",
+        bounce: 0,
+        duration: 0.7,
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+    closed: {
+      clipPath: "inset(10% 50% 90% 50% round 10px)",
+      transition: {
+        type: "spring",
+        bounce: 0,
+        duration: 0.3,
+      },
+    },
+  };
+
+  const itemVariants = {
+    open: {
+      opacity: 1,
+      y: 0,
+      transition: { type: "spring", stiffness: 300, damping: 24 },
+    },
+    closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+  };
+
   useEffect(() => {
     if (nav) {
       document.body.style.overflow = "hidden";
@@ -21,6 +55,22 @@ const Navbar = ({ navLinks }) => {
       document.body.style.overflow = "auto";
     }
   }, [nav]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window?.scrollY > 25 && !border) {
+        setBorder(true);
+      } else {
+        setBorder(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <motion.nav
@@ -35,7 +85,7 @@ const Navbar = ({ navLinks }) => {
       transition={{
         duration: 1,
       }}
-      className="flex h-20 w-full max-w-screen-2xl p-4 md:px-10 lg:px-12 xl:px-20 justify-between items-center bg-zinc-900 text-white fixed top-0 z-50"
+      className="flex h-20 w-full max-w-screen-2xl p-4 md:px-10 lg:px-12 xl:px-20 justify-between items-center text-white fixed top-0 z-50 backdrop-blur-md"
     >
       <button
         onClick={() => !nav && handleClick()}
@@ -44,47 +94,58 @@ const Navbar = ({ navLinks }) => {
         AP
       </button>
 
-      {/* Desktop */}
-      <div className="hidden md:flex flex-row justify-center items-center space-x-8">
-        <ul className="flex flex-row space-x-6">
-          {navLinks?.map((link, index) => (
-            <li
-              key={index}
-              onClick={() => handleClick(link?.ref)}
-              className="cursor-pointer text-white lg:hover:text-green-500"
+      {mobileBreakpoints?.includes(breakpoint) ? (
+        <>
+          <Hamburger size={24} toggled={nav} toggle={setNav} />
+          <motion.div
+            initial={{ opacity: 0, x: "100%" }}
+            animate={nav ? "open" : "closed"}
+            variants={{
+              open: { opacity: 1, x: 0 },
+              closed: { opacity: 1, x: "100%" },
+            }}
+            transition={{
+              duration: 0.5,
+            }}
+            className={`flex flex-col justify-start items-center fixed top-0 right-0 h-screen w-screen bg-zinc-900 mt-20 text-white z-40`}
+          >
+            <motion.ul
+              variants={listVariants}
+              className="flex flex-col h-full w-full space-y-6 justify-center items-center pb-[160px]"
             >
-              {link?.name}
-            </li>
-          ))}
-        </ul>
-        <a
-          href="https://drive.google.com/uc?export=download&id=1EDfXmMCBuh2VwYUhCVJdmFSGd-8FQpa_"
-          rel="noreferrer"
-        >
-          <button className="border-2 border-zinc-100 py-2 px-4 rounded-md text-white lg:hover:text-green-500 lg:hover:border-green-500">
-            Resume
-          </button>
-        </a>
-      </div>
-
-      {/* Mobile */}
-      <div className="md:hidden">
-        <Hamburger size={24} toggled={nav} toggle={setNav} />
-      </div>
-
-      <CSSTransition in={nav} timeout={500} classNames="nav" unmountOnExit>
-        <div
-          className={`flex flex-col justify-start items-center fixed top-0 right-0 h-full w-96 bg-zinc-900 mt-20 text-white z-50 md:hidden`}
-        >
-          <ul className="flex flex-col space-y-6 pt-24 items-center">
+              {navLinks?.map((link, index) => (
+                <motion.li
+                  key={index}
+                  variants={itemVariants}
+                  onClick={() => {
+                    handleClick(link?.ref);
+                    setNav((state) => !state);
+                  }}
+                  className="cursor-pointer text-white text-lg lg:hover:text-green-500"
+                >
+                  {link?.name}
+                </motion.li>
+              ))}
+              <motion.li variants={itemVariants} className="pt-4">
+                <a
+                  href="https://drive.google.com/uc?export=download&id=1EDfXmMCBuh2VwYUhCVJdmFSGd-8FQpa_"
+                  rel="noreferrer"
+                  className="h-full mt-6 border-2 border-zinc-100 py-2 px-4 rounded-md text-white"
+                >
+                  Resume
+                </a>
+              </motion.li>
+            </motion.ul>
+          </motion.div>
+        </>
+      ) : (
+        <div className="flex flex-row justify-center items-center space-x-8">
+          <ul className="flex flex-row space-x-6">
             {navLinks?.map((link, index) => (
               <li
                 key={index}
-                onClick={() => {
-                  handleClick(link?.ref);
-                  setNav((state) => !state);
-                }}
-                className="cursor-pointer text-white text-lg lg:hover:text-green-500"
+                onClick={() => handleClick(link?.ref)}
+                className="cursor-pointer text-white lg:hover:text-green-500"
               >
                 {link?.name}
               </li>
@@ -94,12 +155,16 @@ const Navbar = ({ navLinks }) => {
             href="https://drive.google.com/uc?export=download&id=1EDfXmMCBuh2VwYUhCVJdmFSGd-8FQpa_"
             rel="noreferrer"
           >
-            <button className="mt-6 border-2 border-zinc-100 py-2 px-4 rounded-md text-white lg:hover:text-green-500 lg:hover:border-green-500">
+            <button className="border-2 border-zinc-100 py-2 px-4 rounded-md text-white lg:hover:text-green-500 lg:hover:border-green-500">
               Resume
             </button>
           </a>
         </div>
-      </CSSTransition>
+      )}
+
+      {border && (
+        <div className="absolute bottom-0 h-0.5 w-full rounded-full bg-gradient-to-r from-transparent via-zinc-500 to-transparent mt-2" />
+      )}
     </motion.nav>
   );
 };
