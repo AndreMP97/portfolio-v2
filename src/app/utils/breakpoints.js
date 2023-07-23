ebb725d630c831e488fdd0c3695fd7c38d7c9017
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-07-23 15:14:10 +0100

    create hook helper

---
+++ src/app/utils/breakpoints.js
@@ -0,0 +1,8 @@
+import useBreakpoint from "use-breakpoint";
+
+const BREAKPOINTS = { xs: 0, sm: 640, md: 768, lg: 1024, xl: 1280, xxl: 1536 };
+
+export const CurrentBreakpoint = () => {
+  const { breakpoint } = useBreakpoint(BREAKPOINTS);
+  return breakpoint;
+};
