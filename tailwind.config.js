2d8997f8fdc9c77736ba62196d11d866e5d66bb0
André Pacheco <andre.m.pacheco.97@gmail.com>
2024-04-25 14:03:34 +0100

    Add fonts to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      greatVibes: ['"Great Vibes"', "cursive"],
+      poppins: ['"Poppins"', "sans-serif"],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
