0dc29c90f153c6f257dfeffbbc5dc18d113f224d
André Pacheco <andre.m.pacheco.97@gmail.com>
2024-04-25 14:02:23 +0100

    Fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
+// Styles
 import "@/styles/globals.css";
+// Fonts
+import "@fontsource/great-vibes";
+import "@fontsource/poppins";
 
 const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
